0997f1b716c44ecfa47e8bd424dc9b994c370a58
Bahilu Wendwesen <1bahiluwendwesen@gmail.com>
2024-02-01 21:26:24 -0800

    firsttailwindtrail

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Hedd from './compo/Hedd';
+function App() {
+  return (
+    < div className="App   ">
+      <header className="App-header">
+       
+      
+
+<h1 className="text-3xl font-bold underline bg-clip-border p-6 bg-violet-600 border-4 border-violet-300 border-dashed">  Hello world! </h1>
+
+  <Hedd className="text-3xl font-bold underline"/>
+
+  
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/compo/Hedd.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Hedd() {
+  return (
+    <div className=" text-5xl font-extrabold h-14 bg-gradient-to-r from-sky-500 to-indigo-500" >
+        
+       
+<p className=" bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500"> hi bahilu wendwesen this IS YOUR first trials oky </p>
+<div>
+<h1 className='text-center'> rotete </h1>
+</div>
+
+
+<div class="flex items-center">
+<img src="../image/profile-img.jpg" alt='myimge'/>
+  <div>
+    <strong>Andrew Alfred</strong>
+    <span>Technical advisor</span>
+  </div>
+</div>
+
+</div>
+  )
+}
+
+export default Hedd
